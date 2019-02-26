08652ce75e4163cff6abd16c37b017b4e841d4d1
YeshengC <yesheng250416@gmail.com>
2019-02-26 14:12:32 -0600

    New commands for user input

--- WeatherProgram/src/weatherGrabbing/WeatherDriver.java
+++ WeatherProgram/src/weatherGrabbing/WeatherDriver.java
@@ -10,19 +10,14 @@ import org.json.JSONException;
 public class WeatherDriver {
 	public static void main(String[] args)throws MalformedURLException,
     ProtocolException, IOException, JSONException  {
-		System.out.print("Where are you? ");
+		System.out.println("Where are you? "
+			+ "Please only enter city names for cities in US.");
+		System.out.println("For cities outside of US, please enter city names follows by a comma and then "
+				+ "the two letter abberivation of the country name");
 		//prep for scanning information
 		Scanner userInput = new Scanner(System.in);
 		//scan and store user input
-		String temp = userInput.nextLine();
-		StringBuilder userCity = new StringBuilder();
-		//cleaning out the state abbr.
-		String[] splited = temp.split("\\s+");
-		for (int i=0; i < splited.length ; i++ ) {
-			if (splited[i].length() !=2) {
-				userCity.append(splited[i]+ " ");
-			}
-		}
+		String userCity = userInput.nextLine();
 		System.out.println(userCity+" weather: ");
 		//prep for extracting data
 		WeatherGrabbing weather = new WeatherGrabbing();
